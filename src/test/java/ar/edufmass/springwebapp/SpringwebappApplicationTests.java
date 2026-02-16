853e85b641d3d4b853af8395d12b93d2436e63ba
edufmass <git@edufmass.ar>
2026-02-16 18:38:48 -0300

    first commit

---
+++ src/test/java/ar/edufmass/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edufmass.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
